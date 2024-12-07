12c02d4dd66111f0640a8ef8d263fcc5165e929c
Alae RAHMANI <ra7mani.alae@gmail.com>
2024-12-07 09:43:44 +0100

    Fix ESLint errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    reactStrictMode: true,
+    eslint: {
+        dirs: ['pages', 'components', 'hooks', 'lib', 'app'], // Linting for these directories
+    },
+
 };
 
 export default nextConfig;
+
+
