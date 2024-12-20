f80f3f641aaeb38a253672c24af91799d7516494
Alae RAHMANI <ra7mani.alae@gmail.com>
2024-12-20 08:59:06 +0100

    stable

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    // Si vous aviez des configurations spécifiques dans votre .ts, ajoutez-les ici
+}
+
+module.exports = nextConfig
