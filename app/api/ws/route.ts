3ca095b9bbc365f06ff63efd643da17b0d7313a0
Alae RAHMANI <ra7mani.alae@gmail.com>
2024-12-24 23:28:02 +0100

    Correction de overlay

--- app/api/ws/route.ts
+++ app/api/ws/route.ts
@@ -5,7 +5,7 @@ import { NextRequest } from 'next/server';
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
